4b7b057a5f7110a4b6a385ba4f98a9f10a9495b7
Malith Promoddaya Rathnayake <151937260+malithpromo@users.noreply.github.com>
2024-05-05 21:51:52 +0530

    Update App.js

--- gearwise/src/App.js
+++ gearwise/src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Topbar from './Components/Topbar/Topbar';
-import Navbar from './Components/Navbar/Navbar';
-import Test from './Components/Carousel/Test';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Topbar from "./Components/Topbar/Topbar";
+import Navbar from "./Components/Navbar/Navbar";
+import Test from "./Components/Carousel/Test";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-        <Topbar/>
-        <Navbar/>
-        <Test/>
+      <Topbar />
+      <Navbar />
+      <Test />
     </div>
   );
 }
