59ebbd744c76aa6b293178a60da7e83836d490fd
R.D.K.S. Kumara <134190302+sanjayakasun@users.noreply.github.com>
2024-05-05 13:43:39 +0530

    ui

--- gearwise/src/App.js
+++ gearwise/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Topbar from './Components/Topbar/Topbar';
 import Navbar from './Components/Navbar/Navbar';
+import Carousel from './Components/Carousel/Carousel';
 
 function App() {
   return (
     <div className="App">
         <Topbar/>
         <Navbar/>
+        <Carousel/>
     </div>
   );
 }
