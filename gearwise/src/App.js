d9a4c67f27db74fbf9174f2527bf41cae8c9eabe
R.D.K.S. Kumara <134190302+sanjayakasun@users.noreply.github.com>
2024-05-05 22:06:17 +0530

    silder correction

--- gearwise/src/App.js
+++ gearwise/src/App.js
@@ -2,18 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Topbar from './Components/Topbar/Topbar';
 import Navbar from './Components/Navbar/Navbar';
-import Carousel from './Components/Carousel/Carousel';
 import Aboutus from './Components/Aboutus/Aboutus';
 import Service from './Components/Service/Service';
 import Packages from './Components/Packages/Packages';
 import Location from './Components/Location/Location';
 import Reviews from './Components/Reviews/Reviews';
+import Test from './Components/Carousel/Test';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
         <Topbar/>
         <Navbar/>
+        <Test/>
         <Aboutus/>
         <Service/>
         <Packages/>
