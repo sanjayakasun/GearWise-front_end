e42d8e4642577774c16e19527ed629783b298756
R.D.K.S. Kumara <134190302+sanjayakasun@users.noreply.github.com>
2024-06-06 00:45:29 +0530

    make appointment

--- gearwise/src/Pages/Appointments.js
+++ gearwise/src/Pages/Appointments.js
@@ -1,35 +1,289 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import axios from 'axios';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import '../Components/Appointmentform/Appointmentform.css'
 import Navbar from '../Components/Navbar/Navbar'
 import Topbar from '../Components/Topbar/Topbar'
 import Pageheader from '../Components/Pageheader/Pageheader'
 import Fotter from '../Components/Fotter/Fotter'
-import Contactinfo from '../Components/Appointmentform/Contactinfo'
-import Vehicleinfo from '../Components/Appointmentform/Vehicleinfo'
-import Serviceinfo from '../Components/Appointmentform/Serviceinfo'
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import Packages from '../Components/Packages/Packages'
-import Timeslot from '../Components/Appointmentform/Timeslot'
 import Appointmentbtn from '../Components/Appoinmentbtn/Appointmentbtn'
 
 export default function Appointments() {
+
+  const customerId = "665e144096c5017136fb33a0"
+
+  const [customer, setCustomer] = useState([])
+  const [vehicleType, setVehicleType] = useState("")
+  const [vehicleModel, setVehicleModel] = useState("")
+  const [mfYear, setMFYear] = useState("")
+  const [vrNo, setVRNo] = useState("")
+  const [serviceType, setWashingPlan] = useState("")
+  const [timeSlot, setTimeSlot] = useState("")
+
+  const handleSelectChangeWashingPlan = (event) => {
+    setWashingPlan(event.target.value);
+  };
+  const handleSelectChangeVehicleType = (event) => {
+    setVehicleType(event.target.value);
+  };
+  const handleSelectTimeSlot = (event) => {
+    setTimeSlot(event.target.value);
+  };
+  const [date, setSelectedDate] = useState(null);
+  const handleDateChange = date => {
+    setSelectedDate(date);
+  };
+  console.log('Selected date:', date);
+
+  const createAppointment = async (e)=>{
+    e.preventDefault();
+    let result = await fetch('http://localhost:4005/api/appointments/createappointment',{
+      method:'post',
+      body: JSON.stringify({customerId,vehicleType,vehicleModel,mfYear,vrNo,serviceType,timeSlot,date}),
+      headers:{
+        'Content-Type' : 'application/json'
+      },
+    })
+    result = await result.json
+  }
+
+  // getting customer info using database
+  useEffect(() => {
+    //for testing only 665e144096c5017136fb33a0 otherwise remove the id
+    axios.get('http://localhost:4005/api/customers/customerspro/' + customerId)
+      .then(customer => setCustomer(customer.data))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <div>
-    <Topbar/>
-    <Navbar/>
-    <Pageheader/>
-    
-    <form>
-    <Contactinfo/>
-    <br/>
-    <Vehicleinfo/>
-    <br/>
-    <Serviceinfo/>
-    <br/>
-    <Timeslot/>
-    <br/>
-    <Appointmentbtn/>
-    </form>
-  
-    <Fotter/>
+      <Topbar />
+      <Navbar />
+      <Pageheader />
+
+      <form onSubmit={createAppointment}>
+        <div>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="section-header text-left">
+                  <center><p>Appointment</p></center>
+                </div>
+                <Card className="text-center">
+                  <div className="row">
+                    <h4>Contact Information</h4>
+                    <div className="col-md-6">
+                      <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                          <Form.Floating>
+                            <Form.Control
+                              id="floatingPasswordCustom"
+                              type="text"
+                              value={customer.name}
+                            />
+                            <label htmlFor="floatingPasswordCustom">Name<span>*</span> </label>
+                          </Form.Floating>
+                        </Card.Text>
+                      </Card.Body>
+                    </div>
+                    <div className="col-md-6">
+                      <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                          <Form.Floating>
+                            <Form.Control
+                              id="floatingPasswordCustom"
+                              type="text"
+                              value={customer.email}
+                            />
+                            <label htmlFor="floatingPasswordCustom">E-mail<span>*</span> </label>
+                          </Form.Floating>
+                        </Card.Text>
+                      </Card.Body>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                          <Form.Floating>
+                            <Form.Control
+                              id="floatingPasswordCustom"
+                              type="text"
+                              value={customer.phone}
+                            />
+                            <label htmlFor="floatingPasswordCustom">Contact No<span>*</span> </label>
+                          </Form.Floating>
+                        </Card.Text>
+                      </Card.Body>
+                    </div>
+                  </div>
+                </Card>
+                <br />
+                {/* contact part is over  */}
+                <Card className="text-center">
+                  <div className="row">
+                    <h4>Vehicle Information</h4>
+                    <div className="col-md-6">
+                      <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                          <FloatingLabel controlId="floatingSelect" label="Vehicle Type">
+                            <Form.Select aria-label="Floating label select example" value={vehicleType} onChange={handleSelectChangeVehicleType}>
+                              <option value="Car">Car <span>*</span></option>
+                              <option value="Van">Van</option>
+                              <option value="SUV">SUV</option>
+                              <option value="Truck">Truck</option>
+                              <option value="Bus">Bus</option>
+                            </Form.Select>
+                          </FloatingLabel>
+                        </Card.Text>
+                      </Card.Body>
+                    </div>
+                    <div className="col-md-6">
+                      <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                          <Form.Floating className="mb-3">
+                            <Form.Control
+                              id="floatingInputCustom"
+                              type="Text"
+                              placeholder="Model of Vehicle"
+                              value={vehicleModel}
+                              onChange={(e) =>setVehicleModel(e.target.value)}
+                            />
+                            <label htmlFor="floatingInputCustom">Model of Vehicle <span>*</span></label>
+                          </Form.Floating>
+                        </Card.Text>
+                      </Card.Body>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                          <Form.Floating>
+                            <Form.Control
+                              id="floatingPasswordCustom"
+                              type="text"
+                              placeholder="Manufactured Year"
+                              value={mfYear}
+                              onChange={(e) =>setMFYear(e.target.value)}
+                            />
+                            <label htmlFor="floatingPasswordCustom">Manufactured Year</label>
+                          </Form.Floating>
+                        </Card.Text>
+                      </Card.Body>
+                    </div>
+                    <div className="col-md-6">
+                      <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                          <Form.Floating>
+                            <Form.Control
+                              id="floatingPasswordCustom"
+                              type="text"
+                              placeholder="Veicle Registerd Number" required
+                              value={vrNo}
+                              onChange={(e) =>setVRNo(e.target.value)}
+                            />
+                            <label htmlFor="floatingPasswordCustom">Veicle Registerd Number <span>*</span></label>
+                          </Form.Floating>
+                        </Card.Text>
+                      </Card.Body>
+                    </div>
+                  </div>
+                </Card>
+                {/* vehicle infro part is over */}
+
+                <br />
+                <Card className="text-center">
+                  <div className="row">
+                    <h4>Washing Plan</h4>
+                    <div className="col-md-6">
+                      <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                          <FloatingLabel controlId="floatingSelect" label="Service Type">
+                            <Form.Select aria-label="Floating label select example" value={serviceType} onChange={handleSelectChangeWashingPlan}>
+                              <option value="Basic Cleaning">Basic Cleaning <span>*</span></option>
+                              <option value="Premium Cleaning">Premium Cleaning</option>
+                              <option value="Complex Cleaning">Complex Cleaning</option>
+                              <option value="Full Service">Full Service</option>
+                              <option value="Body Wash">Body Wash</option>
+                              <option value="Wheel Alingments">Wheel Alingments</option>
+                              <option value="Other Specilized Service">Other Specilized Service</option>
+                            </Form.Select>
+                          </FloatingLabel>
+                        </Card.Text>
+                      </Card.Body>
+                    </div>
+                  </div>
+                  <Packages />
+                </Card>
+                {/* service type is over */}
+                <br />
+                <Card className="text-center">
+                  <div className="row">
+                    <h4>Select time Slots</h4>
+                    <div className="col-md-6">
+                      <Card.Body>
+                        <Card.Title>Time Slot</Card.Title>
+                        <Card.Text>
+                          <FloatingLabel controlId="floatingSelect" label="Time Sots">
+                            <Form.Select aria-label="Floating label select example" value={timeSlot} onChange={handleSelectTimeSlot}>
+                              <option value="1">08.00 a.m- 09.00 a.m<span>*</span></option>
+                              <option value="2">09.00 a.m- 10.00 a.m</option>
+                              <option value="3">10.00 a.m- 11.00 a.m</option>
+                              <option value="4">11.00 a.m- 12.00 p.m</option>
+                              <option value="5">01.00 p.m- 02.00 p.m</option>
+                              <option value="6">02.00 p.m- 03.00 p.m</option>
+                              <option value="7">03.00 p.m- 04.00 p.m</option>
+                              <option value="8">04.00 p.m- 05.00 p.m</option>
+                              <option value="9">05.00 p.m- 06.00 p.m</option>
+                              <option value="10">06.00 p.m- 07.00 p.m</option>
+                              <option>Off Peak Pricing Time Slot</option>
+                              <option value="11">08.00 p.m - 09.00 p.m</option>
+                              <option value="12">09.00 p.m- 10.00 p.m</option>
+                            </Form.Select>
+                          </FloatingLabel>
+                        </Card.Text>
+                      </Card.Body>
+                    </div>
+                    <div className="col-md-6">
+                      <Card.Body>
+                        <Card.Title>Pick a Date</Card.Title>
+                        <Card.Text>
+                          <Form.Floating className="mb-6">
+                            <DatePicker
+                              id="datePicker"
+                              selected={date}
+                              onChange={handleDateChange}
+                              dateFormat="MM/dd/yyyy" // Format of the displayed date
+                              placeholderText="Select a date" // Placeholder text when no date is selected
+                            />
+                          </Form.Floating>
+                        </Card.Text>
+                      </Card.Body>
+                    </div>
+                  </div>
+                </Card>
+              </div>
+            </div>
+          </div>
+        </div >
+        <br/>
+        <Appointmentbtn/>
+      </form>
+      <Fotter />
 
     </div>
   )
