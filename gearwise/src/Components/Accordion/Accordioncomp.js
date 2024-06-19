4b205e2093e5234cf9b704ade34c3758128e8458
R.D.K.S. Kumara <134190302+sanjayakasun@users.noreply.github.com>
2024-06-20 02:28:48 +0530

    reshedule appointment

--- gearwise/src/Components/Accordion/Accordioncomp.js
+++ gearwise/src/Components/Accordion/Accordioncomp.js
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import DatePicker from 'react-datepicker';
 const moment = require('moment');
 
 
@@ -12,14 +13,47 @@ export default function Accordioncomp() {
   const [modalShow, setModalShow] = React.useState(false);
   const customerId = "665e144096c5017136fb33a0"
   const [appointments, setappointment] = useState([])
-  const [selectedAppointmentId,setSelectedAppointmentId] = useState(null);
+  const [selectedAppointmentId, setSelectedAppointmentId] = useState(null);
+  const [selectedTimeSlot, setSelectedTimeSlot] = useState([]);
+  const [selcetedDate, setSelectedReDate] = useState([]);
+  const [previousTimeSlot, setpreviousSelectedTimeSlot] = useState([]);
+  const [previousdate, setpreviousSelectedReDate] = useState([]);
+  const [timeSlot, setTimeSlot] = useState("");
+  const [date, setSelectedDate] = useState(null);
+
+  // for reshedule appointment
+  const [show, setShow] = useState(false);
+
+  // const handleresheduledata = (retimeslot,redate) =>{
+  //   reshedule();
+  // }
+
+  const handleClose = () => {
+    // save updated data to db
+    setShow(false);
+  }
+  const handleShow = (appointmentId) => {
+    setSelectedAppointmentId(appointmentId)
+    // lodaing prevoius appointmnet data to reshedule form
+    setShow(true);
+  }
+
+  // const handleSelectTimeSlot = (event) => {
+  // // setting new selected values for resheduling
+  //   setSelectedTimeSlot(event.target.value);
+  // };
+  // const [date, setSelectedDate] = useState(null);
+  // const handleDateChange = date => {
+  //   setSelectedDate(date);
+  // };
+  // end for reshedule appointment
 
 
-function formatDateTime(isoString) {
-  return moment(isoString).format('YYYY/MM/DD');
-  //return moment(isoString).format('MMMM Do YYYY, h:mm:ss a');
-}
-// Example usage
+  function formatDateTime(isoString) {
+    return moment(isoString).format("MM/DD/yyyy");
+    //return moment(isoString).format('MMMM Do YYYY, h:mm:ss a');
+  }
+  // Example usage
 
   useEffect(() => {
     //for testing only 665e144096c5017136fb33a0 otherwise remove the id
@@ -31,15 +65,15 @@ function formatDateTime(isoString) {
     setModalShow(true);
     setSelectedAppointmentId(id);
 
-};
+  };
   const appointmentId = selectedAppointmentId;
   // console.log("appointmnet id",appointmentId);
-
-  function MyVerticallyCenteredModal(props) { // used for msg modal
+  // used for msg modal
+  function MyVerticallyCenteredModal(props) {
 
     const updateStatus = async (status) => {
       try {
-        const response = await axios.put('http://localhost:4005/api/appointments/cancelappointmnet/'+appointmentId, { status: status });
+        const response = await axios.put('http://localhost:4005/api/appointments/cancelappointmnet/' + appointmentId, { status: status });
         console.log(response.data); // Optionally handle the response
         props.onHide(); // Hide the modal after updating the status
         window.location.reload();
@@ -47,7 +81,7 @@ function formatDateTime(isoString) {
         console.error('Error updating appointment status', error);
       }
     };
-  
+
     const handleyesClick = () => {
       updateStatus('Cancelled'); // Change the status to 'Cancelled' when the button is clicked
     };
@@ -72,11 +106,97 @@ function formatDateTime(isoString) {
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={handleyesClick} variant='danger'>Yes</Button>
-          <Button onClick={props.onHide}>No</Button>
+          <Button onClick={props.onHide} variant='success'>No</Button>
         </Modal.Footer>
       </Modal>
     );
   }
+  // end of msd modal for cancelling appointment 
+
+
+  function Example() {
+    const appointmentId = selectedAppointmentId;
+    const [date, setSelectedDate] = useState(null);
+    const [selectedTimeSlot, setSelectedTimeSlot] = useState([]);
+    const handleresheduledata = (timeSlot,date) =>{
+      reshedule(timeSlot,date);
+    }
+    const handleSelectTimeSlot = (event) => {
+      // setting new selected values for resheduling
+        setSelectedTimeSlot(event.target.value);
+      };
+
+      const handleDateChange = date => {
+        setSelectedDate(date);
+      };
+      console.log("slected new date",date);
+      console.log("Selected tinmeslot",selectedTimeSlot);
+
+    const reshedule = async (timeSlot,date) => {
+      console.log(timeSlot);
+      console.log(date);
+      try {
+        const responsereshedule = await axios.put('http://localhost:4005/api/appointments/resheduleappointmnet/' + appointmentId, { timeSlot:timeSlot,date:date });
+        console.log(responsereshedule.data); // Optionally handle the response
+        setShow(false);// Hide the modal after updating the status
+        window.location.reload();
+      } catch (error) {
+        console.error('Error resheduling appointment', error);
+      }
+    };
+    return (
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Time Slot</Form.Label>
+              <Form.Select aria-label="Floating label select example" value={selectedTimeSlot} onChange={handleSelectTimeSlot}>
+                <option value="08.00 a.m- 09.00 a.m">08.00 a.m- 09.00 a.m<span>*</span></option>
+                <option value="09.00 a.m- 10.00 a.m">09.00 a.m- 10.00 a.m</option>
+                <option value="10.00 a.m- 11.00 a.m">10.00 a.m- 11.00 a.m</option>
+                <option value="11.00 a.m- 12.00 p.m">11.00 a.m- 12.00 p.m</option>
+                <option value="01.00 p.m- 02.00 p.m">01.00 p.m- 02.00 p.m</option>
+                <option value="02.00 p.m- 03.00 p.m">02.00 p.m- 03.00 p.m</option>
+                <option value="03.00 p.m- 04.00 p.m">03.00 p.m- 04.00 p.m</option>
+                <option value="04.00 p.m- 05.00 p.m">04.00 p.m- 05.00 p.m</option>
+                <option value="05.00 p.m- 06.00 p.m">05.00 p.m- 06.00 p.m</option>
+                <option value="06.00 p.m- 07.00 p.m">06.00 p.m- 07.00 p.m</option>
+                <option>Off Peak Pricing Time Slot</option>
+                <option value="08.00 p.m - 09.00 p.m">08.00 p.m - 09.00 p.m</option>
+                <option value="09.00 p.m- 10.00 p.m">09.00 p.m- 10.00 p.m</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Date</Form.Label><br />
+              <DatePicker
+                              id="datePicker"
+                              selected={date}
+                              onChange={handleDateChange}
+                              dateFormat="MM/dd/yyyy" // Format of the displayed date
+                              placeholderText="Select a date" // Placeholder text when no date is selected
+                            />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="success" onClick={()=>handleresheduledata(selectedTimeSlot,date)}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
+  // start of resheduling/editing of appointment
+
 
 
   return (
@@ -96,15 +216,15 @@ function formatDateTime(isoString) {
                     const createdate = appointment.createtime;
                     const formattedDate = formatDateTime(isoString);
                     const datetohidecancelbutton = new Date();
-                    console.log("today ",datetohidecancelbutton)
-                    console.log("create date ",createdate)
+                    console.log("today ", datetohidecancelbutton)
+                    console.log("create date ", createdate)
                     const checkbutton = datetohidecancelbutton === createdate
-                    if(checkbutton === 1){
+                    if (checkbutton === 1) {
                       console.log("1")
-                    }else{
+                    } else {
                       console.log('0')
                     }
-                    if(appointment.status === "Active"){
+                    if (appointment.status === "Active") {
                       return (
                         <Accordion >
                           <Accordion.Item eventKey="0">
@@ -129,23 +249,35 @@ function formatDateTime(isoString) {
                                 <Form.Label>Appointment Date</Form.Label>
                                 <Form.Control placeholder="2024/05/16" disabled
                                   value={formattedDate} />
-                                  
+
                               </Form.Group>
+                              <br />
                               <center>
-                              {/* ,{...props.appointment._id} */}
+                                {/* ,{...props.appointment._id} */}
                                 {!checkbutton && <Button variant="danger" onClick={() => handleButtonClick(appointment._id)}>
                                   Cancel Appointment
-                                </Button>}</center>
+                                </Button>} &ensp;
+                                <Button variant="success" onClick={()=>handleShow(appointment._id)}>
+                                  Reshedule appointment
+                                </Button>
+
+                              </center>
+                              {/* used to show the msg model  */}
                               <MyVerticallyCenteredModal
                                 show={modalShow}
                                 onHide={() => setModalShow(false)}
+                              />
+                              {/* used to show the reshedule msg modle */}
+                              <Example
+                                show={show}
+                                onHide={() => setShow(false)}
                               />
                             </Accordion.Body>
                           </Accordion.Item>
                         </Accordion>
                       )
                     }
-                    
+
 
                   })
                 }
