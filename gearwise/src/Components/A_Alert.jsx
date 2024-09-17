52954262003957a2d7738d3d1da3f3bab1c28deb
Malsha Karunarathna <cst20060@std.uwu.ac.lk>
2024-09-17 18:08:37 +0530

    1

--- gearwise/src/Components/A_Alert.jsx
+++ gearwise/src/Components/A_Alert.jsx
@@ -14,7 +14,13 @@ const A_Alert = () => {
                                     </div>
                                     <div>
                                     <div class="relative my-6">
-  <input id="id-date01" type="date" name="id-date01" class="relative w-full h-10 px-4 text-sm placeholder-transparent transition-all border-b outline-none peer border-slate-200 text-slate-500 autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-emerald-500 focus:outline-none invalid:focus:border-pink-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400" />
+     
+
+
+                  <input id="id-date01" 
+                  type="date" name="id-date01" 
+                  min={new Date().toISOString().split("T")[0]} 
+                  class="relative w-full h-10 px-4 text-sm placeholder-transparent transition-all border-b outline-none peer border-slate-200 text-slate-500 autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-emerald-500 focus:outline-none invalid:focus:border-pink-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400" />
   <label for="id-date01" class="absolute -top-2 left-2 z-[1] cursor-text px-2 text-xs text-slate-400 transition-all before:absolute before:left-0 before:top-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-autofill:-top-2 peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:cursor-default peer-focus:text-xs peer-focus:text-emerald-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"> Date </label>
 </div>
 
