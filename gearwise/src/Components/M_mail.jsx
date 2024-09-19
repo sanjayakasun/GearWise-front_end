8104a87029c89fdf8a451e57a5736ad35b1fda94
Malsha Karunarathna <cst20060@std.uwu.ac.lk>
2024-09-19 09:39:38 +0530

    update alert

--- gearwise/src/Components/M_mail.jsx
+++ gearwise/src/Components/M_mail.jsx
@@ -4,50 +4,38 @@ import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Mail = () => {
-    const [products, setProducts] = useState([]);
+    const [vehicles, setVehicles] = useState([]);
 
+    // Fetch vehicle data when the component mounts
     useEffect(() => {
-        const fetchProducts = async () => {
+        const fetchVehicles = async () => {
             try {
-                const response = await axios.get('http://localhost:4005/api/products');
-                const suppliersResponse = await axios.get('http://localhost:4005/api/customers/suppliers');
-                const suppliers = suppliersResponse.data;
-
-                const productsWithSupplierDetails = response.data.map(product => {
-                    const supplier = suppliers.find(supplier => supplier._id === product.s_name);
-                    const supplierName = supplier ? supplier.name : 'Unknown Supplier';
-                    const supplierEmail = supplier ? supplier.email : 'Unknown Email';
-                    return { ...product, supplierName, supplierEmail, alertSent: false }; 
-                });
-
-                setProducts(productsWithSupplierDetails);
+                const response = await axios.get('http://localhost:4005/api/vehicles/');
+                setVehicles(response.data);
             } catch (error) {
-                console.error("There was an error fetching the products!", error);
-                toast.error("There was an error fetching the products!");
+                toast.error('Failed to fetch vehicles');
+                console.error('Error fetching vehicles:', error);
             }
         };
 
-        fetchProducts();
+        fetchVehicles();
     }, []);
 
-    const handleAlert = async (email, productId) => {
+    const handleSendEmailAndDelete = async (vehicleId, email) => {
         try {
-            await axios.post('http://localhost:4005/api/send-email', {
-                to: email,
-                subject: 'Low Product Quantity - Request for Immediate Supply.',
-                text: `Our current stock of your product is running low. Kindly arrange for immediate supply. \n\nThank you.`
-            });
-            toast.success('Alert email sent successfully');
+            // Send email request
+            await axios.post('http://localhost:4005/api/send-email', { email });
 
-            // Update the state to mark the alert as sent for the specific product
-            setProducts(prevProducts =>
-                prevProducts.map(product =>
-                    product._id === productId ? { ...product, alertSent: true } : product
-                )
-            );
+            // Delete vehicle request
+            await axios.delete(`http://localhost:4005/api/vehicles/${vehicleId}`);
+
+            // Remove the vehicle from the UI
+            setVehicles(vehicles.filter(vehicle => vehicle._id !== vehicleId));
+
+            toast.success('Email sent and vehicle deleted successfully');
         } catch (error) {
-            console.error('Error sending alert email', error);
-            toast.error('Failed to send alert email');
+            toast.error('Failed to send email or delete vehicle');
+            console.error('Error:', error);
         }
     };
 
@@ -59,86 +47,73 @@ const Mail = () => {
                         <div className="p-1.5 min-w-full inline-block align-middle">
                             <div className="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden dark:bg-neutral-800 dark:border-neutral-700">
                                 <div className="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-b border-gray-200 dark:border-neutral-700">
-                                    <div>
-                                        <h3 className="text-center mb-4 text-5xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl dark:text-white">
-                                            <span className="text-blue-800 dark:text-blue-500">Supplier </span> Product <span className="text-blue-800 dark:text-blue-500">Details</span>
-                                        </h3>
-                                    </div>
+                                    <h3 className="text-center mb-4 text-5xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl dark:text-white">
+                                        <span className="text-blue-800 dark:text-blue-500">Vehicle</span> Alert
+                                    </h3>
                                 </div>
                                 <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
                                     <thead className="bg-gray-50 dark:bg-gray-800">
                                         <tr>
                                             <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                                Product Name
-                                            </th><th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                            Supplier Name
-                                            </th>
-                                            
-                                            <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                                Supplied Date
+                                                Vehicle Number
                                             </th>
                                             <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                                Quantity
+                                                Owner
                                             </th>
                                             <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                                Unit Price
+                                                Service Date
+                                            </th>
+                                            <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                Service Type
+                                            </th>
+                                            <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                Vehicle Type
                                             </th>
                                             <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                                 Action
                                             </th>
-                                            
                                         </tr>
                                     </thead>
                                     <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-                                        {products.length > 0 ? (
-                                            products.map(product => (
-                                                <tr key={product._id}>
+                                        {vehicles.length > 0 ? (
+                                            vehicles.map(vehicle => (
+                                                <tr key={vehicle._id}>
                                                     <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                                                        <h2 className="text-sm font-normal">{product.name}</h2>
-                                                    </td>
-                                                    <td className="size-px whitespace-nowrap" style={{ paddingLeft: '20px' }}>
-                                                        <div className="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3">
-                                                            <div className="flex items-center gap-x-3">
-                                                                <div className="grow">
-                                                                    <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-200"> {product.supplierName} </span>
-                                                                    <span className="block text-sm text-gray-500 dark:text-neutral-500"> {product.supplierEmail} </span>
-                                                                </div>
-                                                            </div>
-                                                        </div>
+                                                        <h2 className="text-sm font-normal">{vehicle.vehicle_no}</h2>
                                                     </td>
                                                     <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                                                        <h2 className="text-sm font-medium text-gray-800 dark:text-white">{new Date(product.date).toLocaleDateString()}</h2>
+                                                        <h2 className="text-sm font-semibold">
+                                                            {vehicle.owner ? vehicle.owner.name : 'Unknown'}
+                                                        </h2>
+                                                        <p className="text-sm text-gray-500 dark:text-neutral-500">
+                                                            {vehicle.owner ? vehicle.owner.email : 'No email'}
+                                                        </p>
                                                     </td>
                                                     <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                                                        <div className={`inline-flex items-center px-3 py-1 rounded-full gap-x-2 ${product.quantity < 100 ? 'text-red-500 bg-red-100/60 dark:bg-gray-800' : 'text-emerald-500 bg-emerald-100/60 dark:bg-gray-800'}`}>
-                                                            <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                                <path d="M10 3L4.5 8.5L2 6" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                                                            </svg>
-                                                            <h2 className="text-sm font-normal">{product.quantity}</h2>
-                                                        </div>
+                                                        <h2 className="text-sm font-medium text-gray-800 dark:text-white">
+                                                            {new Date(vehicle.s_date).toLocaleDateString()}
+                                                        </h2>
+                                                    </td>
+                                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                                                        <h2 className="text-sm font-normal">{vehicle.s_type}</h2>
+                                                    </td>
+                                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                                                        <h2 className="text-sm font-normal">{vehicle.v_type}</h2>
                                                     </td>
                                                     <td className="px-4 py-4 text-sm whitespace-nowrap">
-                                                        <div className="flex items-center gap-x-6">
-                                                            <h2 className="text-sm font-normal">Rs. {product.price}</h2>
-                                                        </div>
+                                                        <button
+                                                            className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none"
+                                                            onClick={() => handleSendEmailAndDelete(vehicle._id, vehicle.owner ? vehicle.owner.email : '')}
+                                                        >
+                                                            Alert
+                                                        </button>
                                                     </td>
-                                                    <td className="px-4 py-4 text-sm whitespace-nowrap">
-                                                        <div className="flex items-center gap-x-6">
-                                                            <button
-                                                                className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none"
-                                                                onClick={() => handleAlert(product.supplierEmail, product._id)}
-                                                            >
-                                                                Alert
-                                                            </button>
-                                                        </div>
-                                                    </td>
-                                                   
                                                 </tr>
                                             ))
                                         ) : (
                                             <tr>
-                                                <td colSpan="7" className="px-4 py-4 text-center text-sm text-gray-500 dark:text-gray-300">
-                                                    No products found.
+                                                <td colSpan="6" className="px-4 py-4 text-center text-sm text-gray-500 dark:text-gray-300">
+                                                    No vehicles found.
                                                 </td>
                                             </tr>
                                         )}
