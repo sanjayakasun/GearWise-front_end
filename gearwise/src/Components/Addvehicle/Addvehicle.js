6bd9d4690cc5394872254d2630cf44fa1f3601f1
Hasanki Dharmarathna <didulanidharmarathna@gmail.com>
2024-10-04 21:12:31 +0530

    updated2

--- gearwise/src/Components/Addvehicle/Addvehicle.js
+++ gearwise/src/Components/Addvehicle/Addvehicle.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import { toast, ToastContainer } from 'react-toastify';  
+import 'react-toastify/dist/ReactToastify.css';  
 import Img1 from '../../img/Addvehicle1.jpg';
 
-export default function Addvehicle() {
+export const Addvehicle = () => {
+    // State to store form data
+    const [vehicleData, setVehicleData] = useState({
+        vehicleType: '',
+        vehicleModel: '',
+        mfYear: '',  
+        vrNo: '' 
+    });
+
+    // Handle input changes
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setVehicleData({ ...vehicleData, [name]: value });
+    };
+
+    // Handle form submission
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:4005/api/vehicles/newVehicle', {
+                ...vehicleData,
+                customerId: "665e144096c5017136fb33a0" // Example customerId, replace with actual ID
+            });
+            // Show success toast message
+            toast.success('Vehicle added successfully!', {
+                
+            });
+            console.log('Vehicle added successfully:', response.data);
+        } catch (error) {
+            // Show error toast message
+            toast.error('Error adding vehicle. Please try again.', {
+                
+            });
+            console.error('Error adding vehicle:', error.response ? error.response.data : error.message);
+        }
+    };
+
     return (
         <div>
+            
+            <ToastContainer />
+
             <style>{`
                 .red-icon {
                     color: red;
@@ -14,14 +56,12 @@ export default function Addvehicle() {
                 .button-container {
                     display: flex;
                     justify-content: flex-end;
-                    margin-right: 45px; /* Adjust this value to move the button to the left */
-                    margin-bottom: 20px; /* Adjust this value to add space below the button */
+                    margin-right: 45px; 
+                    margin-bottom: 20px; 
                 }
             `}</style>
             <div className="container">
                 <div className="row align-items-center">
-                    {/* Profile change start */}
-
                     <div className="col-lg-5">
                         <div className="section-header text-left">
                             <p>Vehicle Space</p>
@@ -29,98 +69,95 @@ export default function Addvehicle() {
                         </div>
                         <div className="about-content">
                             <p>
-                                Manage your entire vehicle fleet with ease at <strong>GearWise! </strong>
+                                Manage your entire vehicle fleet with ease at <strong>GearWise!</strong>
                                 Here, you can effortlessly add details for each of your vehicles.
                             </p>
                             <ul>
-                                <li><i className="far fa-check-circle red-icon"></i>Easily manage multiple vehicles: Add information for all your vehicles in one place.</li>
-                                <li><i className="far fa-check-circle red-icon"></i> Ensure that every vehicle’s details are captured, providing a complete overview of your entire fleet.</li>
-                                {/* <li><i className="far fa-check-circle red-icon"></i> Make It Yours</li> */}
+                                <li><i className="far fa-check-circle red-icon"></i> Easily manage multiple vehicles.</li>
+                                <li><i className="far fa-check-circle red-icon"></i> Capture every vehicle's details.</li>
                             </ul>
-                            
-                            {/* {!hideButton && <a className="btn btn-custom" href="/Reward">Learn More</a>} */}
                         </div>
-                        <img src={Img1} alt="Image" style={{ width: '450px', height: 'auto' }} />
+                        <img src={Img1} alt="Vehicle Space" style={{ width: '450px', height: 'auto' }} />
                     </div>
 
                     <div className="col-lg-7" style={{ paddingBottom: '60px' }}>
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-lg-12">
-                                    <Card className="text-center">
-                                        <div className="row">
-                                            <h4>Vehicle Information</h4>
-                                            <Card.Body>
-                                                <Card.Title></Card.Title>
-                                                <Card.Text>
-                                                    <FloatingLabel controlId="floatingSelect" label="Vehicle Type">
-                                                        <Form.Select aria-label="Floating label select example">
-                                                            <option value="1">Car <span>*</span></option>
-                                                            <option value="2">Van</option>
-                                                            <option value="3">SUV</option>
-                                                            <option value="3">Truck</option>
-                                                            <option value="3">Bus</option>
-                                                        </Form.Select>
-                                                    </FloatingLabel>
-                                                </Card.Text>
-                                            </Card.Body>
-                                            <Card.Body>
-                                                <Card.Title></Card.Title>
-                                                <Card.Text>
-                                                    <Form.Floating className="mb-3">
-                                                        <Form.Control
-                                                            id="floatingInputCustom"
-                                                            type="Text"
-                                                            placeholder="Model of Vehicle"
-                                                        />
-                                                        <label htmlFor="floatingInputCustom">Model of Vehicle <span>*</span></label>
-                                                    </Form.Floating>
-                                                </Card.Text>
-                                            </Card.Body>
-                                        </div>
-                                        <div className="row">
-                                            <Card.Body>
-                                                <Card.Title></Card.Title>
-                                                <Card.Text>
-                                                    <Form.Floating>
-                                                        <Form.Control
-                                                            id="floatingPasswordCustom"
-                                                            type="text"
-                                                            placeholder="Manufactured Year"
-                                                        />
-                                                        <label htmlFor="floatingPasswordCustom">Manufactured Year</label>
-                                                    </Form.Floating>
-                                                </Card.Text>
-                                            </Card.Body>
-                                            <Card.Body>
-                                                <Card.Title></Card.Title>
-                                                <Card.Text>
-                                                    <Form.Floating>
-                                                        <Form.Control
-                                                            id="floatingPasswordCustom"
-                                                            type="text"
-                                                            placeholder="Vehicle Registered Number" required
-                                                        />
-                                                        <label htmlFor="floatingPasswordCustom">Vehicle Registered Number <span>*</span></label>
-                                                    </Form.Floating>
-                                                </Card.Text>
-                                            </Card.Body>
-                                        </div>
-                                        <div className="button-container">
-                                            <button type="submit" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-800 disabled:opacity-50 disabled:pointer-events-none">
-                                                Add Vehicle
-                                            </button>
-                                        </div>
-                                    </Card>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Profile change end */}
+                        <Card className="text-center">
+                            <form onSubmit={handleSubmit}>
+                                <div className="row">
+                                    <h4>Vehicle Information</h4>
+                                    <Card.Body>
+                                        <FloatingLabel controlId="floatingSelect" label="Vehicle Type">
+                                            <Form.Select
+                                                aria-label="Vehicle Type"
+                                                name="vehicleType"
+                                                value={vehicleData.vehicleType}
+                                                onChange={handleChange}
+                                            >
+                                                <option value="">Select Vehicle Type</option>
+                                                <option value="Car">Car</option>
+                                                <option value="Van">Van</option>
+                                                <option value="SUV">SUV</option>
+                                                <option value="Truck">Truck</option>
+                                                <option value="Bus">Bus</option>
+                                            </Form.Select>
+                                        </FloatingLabel>
+                                    </Card.Body>
 
-                    {/* Profile Form end */}
+                                    <Card.Body>
+                                        <Form.Floating className="mb-3">
+                                            <Form.Control
+                                                id="vehicleModel"
+                                                type="text"
+                                                placeholder="Model of Vehicle"
+                                                name="vehicleModel"
+                                                value={vehicleData.vehicleModel}
+                                                onChange={handleChange}
+                                            />
+                                            <label htmlFor="vehicleModel">Model of Vehicle </label>
+                                        </Form.Floating>
+                                    </Card.Body>
+                                </div>
+                                <div className="row">
+                                    <Card.Body>
+                                        <Form.Floating>
+                                            <Form.Control
+                                                id="mfYear"
+                                                type="text"
+                                                placeholder="Manufactured Year"
+                                                name="mfYear"
+                                                value={vehicleData.mfYear}
+                                                onChange={handleChange}
+                                            />
+                                            <label htmlFor="mfYear">Manufactured Year</label>
+                                        </Form.Floating>
+                                    </Card.Body>
+                                    <Card.Body>
+                                        <Form.Floating>
+                                            <Form.Control
+                                                id="vrNo"
+                                                type="text"
+                                                placeholder="Vehicle Registered Number"
+                                                name="vrNo"
+                                                value={vehicleData.vrNo}
+                                                onChange={handleChange}
+                                                required
+                                            />
+                                            <label htmlFor="vrNo">Vehicle Registered Number <span>*</span></label>
+                                        </Form.Floating>
+                                    </Card.Body>
+                                </div>
+                                <div className="button-container">
+                                    <button type="submit" onSubmit={handleSubmit} className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-800 disabled:opacity-50">
+                                        Add Vehicle
+                                    </button>
+                                </div>
+                            </form>
+                        </Card>
+                    </div>
                 </div>
             </div>
         </div>
     );
-}
+};
+
+export default Addvehicle;
