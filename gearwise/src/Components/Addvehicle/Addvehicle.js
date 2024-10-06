ebab03e93edf05163fa2c64af4a48075c0f8e131
Hasanki Dharmarathna <didulanidharmarathna@gmail.com>
2024-10-06 18:36:24 +0530

    Update

--- gearwise/src/Components/Addvehicle/Addvehicle.js
+++ gearwise/src/Components/Addvehicle/Addvehicle.js
@@ -236,9 +236,15 @@ export const Addvehicle = () => {
                                         </Form.Floating>
                                     </Card.Body>
                                 </div>
-                                <Button variant="primary" type="submit">
-                                    Add Vehicle
-                                </Button>
+
+                                <div style={{ paddingLeft: '240px' }}>  
+                                    <button
+                                        type="submit"
+                                        className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-800 disabled:opacity-50">
+                                        Add Vehicle
+                                    </button>
+                                </div>
+
                             </form>
                         </Card>
                     </div>
